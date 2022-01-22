980cf84e7ecc2811adaf612c953a5cb5a9f3dbad
Dino Jazvin <dinojaz@uw.edu>
2022-01-21 23:04:51 -0800

    Moving all files outside 'game-engine' folder

---
+++ back.js
@@ -0,0 +1,13 @@
+class back {
+    constructor(game){
+        this.game = game;
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./back.png"), 0, 0, 960, 576, 0, 100000);
+    };
+
+    update(){
+    };
+
+    draw(ctx){
+        this.animator.drawFrame(this.game.clockTick, ctx, 0,0);
+    };
+}
---
+++ sonic.js
@@ -0,0 +1,23 @@
+class sonic {
+    constructor(game){
+        this.game = game;
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./sonic.png"), 0, 0, 38, 50, 8, 0.15);
+        this.x = 0;
+        this.y = 170;
+        this.speed = 200;
+    };
+
+    update(){
+        this.x += this.speed * this.game.clockTick;
+        if(this.x > 800){
+            this.x = 0;
+            this.y = 170;
+        }
+        if(this.x > 400)
+            this.y += .95;
+    };
+
+    draw(ctx){
+        this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    };
+}
